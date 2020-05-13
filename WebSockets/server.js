e910f67a52bf44f44ca2a27def87a0f1a58c7846
itayGuetta <itayguetta@gmail.com>
2020-05-13 23:07:08 +0300

    Server Web sockt update  & view

--- WebSockets/server.js
+++ WebSockets/server.js
@@ -3,11 +3,25 @@ const serv = new websocket({port: 5002});
 
 serv.on('connection' , function(ws){
     ws.on('message', function(massege){
+
+        massege = JSON.parse(massege);
+        if(massege.type == "massage"){
+            ws.personName = massege.data
+        }
+
         console.log("Recived : " + massege);
+
+        serv.clients.forEach(function e(client){
+            if(client != ws)
+                client.send(massege)
+        })
+
+
+
         if(massege == "Hello"){
             ws.send("Hello to you to ! (server :) ) ")
         }else{
-            ws.send(massege)
+            ws.send("From - server : "+massege)
         }
 
     });
